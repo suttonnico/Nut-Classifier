a13eeb9c6d6467dc62c8bd93ea63cc986c0125d4
suttonnico <suttonnico2@gmail.com>
2018-09-03 18:31:20 -0300

    funca

--- rpi_test.py
+++ rpi_test.py
@@ -1,18 +1,27 @@
 import numpy as np
-import matplotlib.pylab as plt
 import cnn
 from keras.models import load_model
 import cv2
+import time
+size = 150
+W = 2*size
+H = 2*size
 
 model = load_model('model.h5')
+weights = model.get_weights()
+my_cnn = cnn.cnn(img_width=W, img_height=H)
 
-
+my_cnn.set_weights(weights)
+dif = 150
 # initialize the camera
 cam = cv2.VideoCapture(0)   # 0 -> index of camera
-s, img = cam.read()
-if s:    # frame captured without any errors
-    cv2.namedWindow("cam-test",cv2.WINDOW_NORMAL)
-    cv2.imshow("cam-test",img)
-    cv2.waitKey(0)
-    cv2.destroyWindow("cam-test")
+N = 1000
+start = time.time()
+for i in range(N):
+    s, img = cam.read()
+    if s:    # frame captured without any errors
+        my_cnn.predict_classes(cv2.resize(img,(2*dif, 2*dif)).reshape([-1,300,300,3]),batch_size=1)
+        print(i)
     #imwrite("filename.jpg",img) #save image
+end = time.time()
+print((end-start)/N)
